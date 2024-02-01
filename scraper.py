e8609623bac42dd44d19efad3cc1dd52c1dffd3e
SusLiu03 <susannl5@uci.edu>
2024-02-01 13:13:53 -0800

    added assignment1 functions

--- scraper.py
+++ scraper.py
@@ -1,5 +1,76 @@
 import re
 from urllib.parse import urlparse
+import sys
+
+
+def tokenize(text_file_path):
+    """
+    reads a text file and returns a list of the tokens in that file
+    The time complexity is O(n) where n is the number of characters in the file
+    This would make it run in polynomial time to the size of the input.
+    """
+    try:
+        token_list = []
+        with open(text_file_path, 'r', encoding='utf-8') as file:
+            new_word = ""
+            letter = file.read(1)
+            while letter:
+                val = ord(letter.lower())
+                if letter.isalnum() and (97 <= val <= 122 or 48 <= val <= 57):
+                    new_word += letter.lower()
+                else:
+                    #if '\n', would still add last word in line
+                    if new_word != "":
+                        token_list.append(new_word)
+                    new_word = ""
+                letter = file.read(1)
+            if new_word != "":
+                token_list.append(new_word)
+        return token_list
+    except OSError:
+        print("Error: Invalid file.")
+        sys.exit()
+    except UnicodeDecodeError:
+        print("Error: Invalid file type.")
+        sys.exit()
+def computeWordFrequencies(token_list):
+    """
+    counts the number of occurrences of each token in the token list
+    O(n) where n is the number of tokens in the list
+    This would make it run in linear time to the size of the input.
+    """
+    token_dict = dict()
+    for token in token_list:
+        if token not in token_dict.keys():
+            token_dict[token] = 1
+        else:
+            token_dict[token] += 1
+    return token_dict
+
+def similarCharacters():
+    if len(sys.argv) != 3:
+        print("Error: Incorrect number of arguments.")
+        sys.exit()
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
+    try:
+        file1_dict = computeWordFrequencies(tokenize(file1))
+        file2_dict = computeWordFrequencies(tokenize(file2))
+    except OSError:
+        print("Error: Invalid file.")
+        sys.exit()
+    except UnicodeDecodeError:
+        print("Error: Invalid file type.")
+        sys.exit()
+    common_tokens = []
+    common_count = 0
+    for key in file1_dict.keys():
+        if key in file2_dict:
+            common_tokens.append(key)
+            common_count += 1
+    # for token in common_tokens:
+    #     sys.stdout.write(f'{token}\n')
+    print(f'{common_count}')
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
