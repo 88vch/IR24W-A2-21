12410df4ddb4378994bcd818e74f61c8c70ed06a
Zachary Qin <zvchqin@gmail.com>
2024-02-08 15:42:42 -0800

    scraper.py; removed '.' to get rid of 'pdf' in path (https://www.informatics.uci.edu/files/pdf/)

--- scraper.py
+++ scraper.py
@@ -146,7 +146,7 @@ def is_valid(url):
     try:
         parsed = urlparse(url)
         # check if url contains "pdf"
-        for extension in {".pdf", ".zip", ".gz", ".css", ".ps", ".ppt", ".js"}:
+        for extension in {"pdf", ".zip", ".gz", ".css", ".ps", ".ppt", ".js", ".bib"}:
             if extension in url:
                 return False
         if parsed.scheme not in set(["http", "https"]):
