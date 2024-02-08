48e3ee76ad03bce24af62b2617e0bd45dba8d448
dannycowles <dannycowless@gmail.com>
2024-02-08 12:47:44 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -147,7 +147,10 @@ def is_valid(url):
             return False
         # Gets the ending of the url
         # filtered_hostname is everything to the right of the first "."
-        filtered_hostname = parsed.hostname.split('.', 1)[1]
+        if '.' not in parsed.hostname:
+            return False
+        else:
+            filtered_hostname = parsed.hostname.split('.', 1)[1]
         if filtered_hostname not in set(["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu"]):
             return False
         return not re.match(
