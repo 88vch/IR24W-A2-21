66f5cb700f465a232cde4c2bb1802ad8601f5e6e
dannycowles <dannycowless@gmail.com>
2024-02-08 21:32:30 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -159,12 +159,12 @@ def is_valid(url):
             return False
 
         # Deal with page traps for example .../page/200 we handle this by setting max page # as 5
-        #if "page/" in parsed.path:
-            #splitparse = parsed.path.split("/")
-            #if splitparse[len(splitparse) - 1] != '':
-                #pagenum = int(splitparse[len(splitparse) - 1])
-                #if pagenum <= 5:
-                    #return False
+        if "page/" in parsed.path:
+            splitparse = parsed.path.split("/")
+            if splitparse[len(splitparse) - 1] != '':
+                pagenum = int(splitparse[len(splitparse) - 1])
+                if pagenum <= 5:
+                    return False
 
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
